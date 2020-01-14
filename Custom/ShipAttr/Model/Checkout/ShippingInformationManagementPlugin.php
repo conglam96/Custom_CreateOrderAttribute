d716103fd6b3e55f741a2e3bb1b954340f8a7f74
Lam Nguyen <lamnguyenptit96143@gmai.com>
2020-01-14 10:08:11 +0700

    create order attribute

---
+++ Custom/ShipAttr/Model/Checkout/ShippingInformationManagementPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Custom\ShipAttr\Model\Checkout;
+
+class ShippingInformationManagementPlugin
+{
+
+    protected $quoteRepository;
+
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository
+    ) {
+        $this->quoteRepository = $quoteRepository;
+    }
+
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+        $extAttributes = $addressInformation->getExtensionAttributes(); //order
+        $orderBy = $extAttributes->getOrderBy();
+        $quote = $this->quoteRepository->getActive($cartId);
+        $quote->setOrderBy($orderBy);
+    }
+}
---
+++ Custom/ShipAttr/Setup/InstallSchema.php
@@ -0,0 +1,48 @@
+<?php
+namespace Custom\ShipAttr\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup,ModuleContextInterface $context)
+    {
+        $installer=$setup;
+        $installer->startSetup();
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('quote'),
+            'order_by',
+            [
+                'type'=>'text',
+                'nullable'=>false,
+                'comment'=>'Order by author'
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('sales_order'),
+            'order_by',
+            [
+                'type'=>'text',
+                'nullable'=>false,
+                'comment'=>'Order by author'
+            ]
+        );
+
+        $installer->getConnection()->addColumn(
+            $installer->getTable('sales_order_grid'),
+            'order_by',
+            [
+                'type'=>'text',
+                'nullable'=>false,
+                'comment'=>'Order by author'
+            ]
+        );
+
+
+        $installer->endSetup();
+    }
+}
